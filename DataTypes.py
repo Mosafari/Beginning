ae136ece168e2e52ebcfe8ce8bc88987c09b23fd
mosafari <m.safari2680@gmail.com>
2022-11-18 20:49:21 +0330

    Data Types

---
+++ DataTypes.py
@@ -0,0 +1,18 @@
+#variables : Variables are containers for storing data values. like : x,y,z,A_a,BreakingBad, ...
+# You can get the data type of any object by using the type() function.
+
+# Numbers
+x = 13 #int
+y= 24.5 #float
+k= 1j #complex
+
+z= 'hello ' #string
+
+i=False #boolean
+
+a= [1,2,3.4,'a','b'] # list to store string and int/float list, ...
+b= ( 1,2,3.4,'a' 'b') # tuple to store string & int/float list, ... (imutable)
+c = {'one': 1,2:'two'} #dict key(imutable): value paires to store val and accses with [key]
+d={1,2,3,4,4,4,'a','b','c'} #set to store string and int/float ... (Ignores duplicate values)
+
+n= None #Nonetype
