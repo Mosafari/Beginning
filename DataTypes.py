a2d3f225bdce1b068447d69560903cb77f67f9c7
mosafari <m.safari2680@gmail.com>
2022-11-18 21:02:38 +0330

    Output Variables

--- DataTypes.py
+++ DataTypes.py
@@ -6,7 +6,8 @@ x = 13 #int
 y= 24.5 #float
 k= 1j #complex
 
-z= 'hello ' #string
+z= 'hello, ' #string
+h= "world!"
 
 i=False #boolean
 
@@ -16,3 +17,18 @@ c = {'one': 1,2:'two'} #dict key(imutable): value paires to store val and accses
 d={1,2,3,4,4,4,'a','b','c'} #set to store string and int/float ... (Ignores duplicate values)
 
 n= None #Nonetype
+
+
+
+# Output Variables
+# The Python print() function is often used to output variables.
+
+print(x) #int
+print(y) #float
+print(k) #complex
+
+print()  #empty line
+
+print(z) 
+print(h)
+print(z+h) #print hello, world! in one line
