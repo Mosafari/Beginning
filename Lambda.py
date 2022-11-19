345b8fa9ab700457f50393c355f1b1873a3bf4db
mosafari <m.safari2680@gmail.com>
2022-11-19 17:27:04 +0330

    Lambda

---
+++ Lambda.py
@@ -0,0 +1,22 @@
+# Lambda
+
+# A lambda function is a small anonymous function.
+# A lambda function can take any number of arguments, but can only have one expression.
+
+# Syntax
+# lambda arguments : expression
+
+# The expression is executed and the result is returned
+
+# lambda power2
+
+p = lambda num : num**2
+
+print(p(2))
+print(p(3))
+print() #empty line
+
+# Why Use Lambda Functions?
+# The power of lambda is better shown when you use them as an anonymous function inside another function.
+
+# Use lambda functions when an anonymous function is required for a short period of time.
