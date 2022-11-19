9b7ce14dab59e92a12caf1874bfe937652058ad2
mosafari <m.safari2680@gmail.com>
2022-11-19 15:53:36 +0330

    If

---
+++ Conditions.py
@@ -0,0 +1,23 @@
+# Conditions and If statements
+
+# Python supports the usual logical conditions from mathematics:
+
+#     Equals: a == b
+#     Not Equals: a != b
+#     Less than: a < b
+#     Less than or equal to: a <= b
+#     Greater than: a > b
+#     Greater than or equal to: a >= b
+
+# These conditions can be used in several ways, most commonly in "if statements" and loops.
+
+# An "if statement" is written by using the if keyword.
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+# Indentation
+# ***Python relies on indentation (whitespace at the beginning of a line) to define scope in the code.
+# Other programming languages often use curly-brackets for this purpose. ***
