e24a6d78c0f5d12ed912946c60c6ba6ce72dfbbe
mosafari <m.safari2680@gmail.com>
2022-11-19 16:36:30 +0330

    The continue Statement

--- Loops/While.py
+++ Loops/While.py
@@ -24,3 +24,15 @@ while i < 10:
     i+=1
     
 print() #empty line
+
+# The continue Statement
+# With the continue statement we can stop the current iteration, and continue with the next:
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+  
+print() #empty line
