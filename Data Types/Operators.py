e861c96560b1449f479a5b90fd327fbcb3be1f01
mosafari <m.safari2680@gmail.com>
2022-11-19 10:01:16 +0330

    Arithmetic Operators

--- Data Types/Operators.py
+++ Data Types/Operators.py
@@ -1,4 +1,11 @@
 # Python Operators
 #  Operators are used to perform operations on variables and values.
 
+# Python Arithmetic Operators
 
+print(4/2) # output : 2
+print(6-2) # output : 4
+print(2+4) # output : 6
+print(4*2) # output : 8
+print(4**2) # output : 16
+print()  #empty line
