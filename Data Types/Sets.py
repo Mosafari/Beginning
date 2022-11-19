1bb682a2f03167f237953f82eace7efb7122f3ec
mosafari <m.safari2680@gmail.com>
2022-11-19 14:30:21 +0330

    Add Items

--- Data Types/Sets.py
+++ Data Types/Sets.py
@@ -7,7 +7,7 @@
 
 # The set() Constructor
 # It is also possible to use the set() constructor to make a set.
-list1=[1,2,2,2,3]
+list1=[1,2,2,2,3,'python','test']
 set0 = set(list1)
 print(set0)
 set1 ={1,2,3,3,3,True,'str','str'}
@@ -15,3 +15,27 @@ print(set1)
 print(len(set1))
 print(type(set1))
 print() #empty line
+
+#  Add Items
+# Once a set is created, you cannot change its items, but you can add new items.
+# To add one item to a set use the add() method.
+
+set1.add(22)
+print(set1)
+print() #empty line
+
+#  Add Sets
+# To add items from another set into the current set, use the update() method.
+
+set1.update(set0)
+print(set1)
+print() #empty line
+
+#  Add Any Iterable
+# The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+
+list2 =[4,5,6,6,6,6,7]
+set1.update(list2)
+print(set1)
+print() #empty line
+
