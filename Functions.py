faaa0c98e4ebcaa054b8d0c8406917f1b493c71d
mosafari <m.safari2680@gmail.com>
2022-11-19 17:17:04 +0330

    Arbitrary Keyword Arguments, **kwargs

--- Functions.py
+++ Functions.py
@@ -60,3 +60,17 @@ def Greeteng3(name1,name2,name3):
     print() #empty line
     
 Greeteng3(name2='ali',name1='mohamad',name3='reza')
+# The phrase Keyword Arguments are often shortened to kwargs in Python documentations.
+
+# Arbitrary Keyword Arguments, **kwargs
+# If you do not know how many keyword arguments that will be passed into your function,
+# add two asterisk: ** before the parameter name in the function definition.
+# This way the function will receive a dictionary of arguments, and can access the items accordingly:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+  print() #empty line
+
+my_function(fname = "Tobias", lname = "Refsnes") 
+
+# Arbitrary Kword Arguments are often shortened to **kwargs in Python documentations.
