bd8fb4eafdc1c3d02eb86c2f87d90dbb8d2d1538
mosafari <m.safari2680@gmail.com>
2022-11-19 16:06:48 +0330

    Short Conditions

--- Conditions.py
+++ Conditions.py
@@ -49,3 +49,22 @@ print() #empty line
 
 # The else keyword catches anything which isn't caught by the preceding conditions
 
+# Short conditions
+# If you have only one statement to execute, you can put it on the same line as the if statement.
+
+j =6
+k =7
+if j<k : print("k is greater")
+print() #empty line
+
+#short if-else
+print('j') if j>k else print("k")
+print() #empty line
+
+# This technique is known as Ternary Operators, or Conditional Expressions.
+
+# You can also have multiple else statements on the same line:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B") 
+print() #empty line
