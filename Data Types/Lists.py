a6530672ff22aa2d6c11071e95f51f74d2bc93b7
mosafari <m.safari2680@gmail.com>
2022-11-19 11:12:37 +0330

    Access List Items

--- Data Types/Lists.py
+++ Data Types/Lists.py
@@ -11,4 +11,32 @@ A = []
 # List items are indexed, the first item has index [0], the second item has index [1] etc.
 
 B = [1,1,2,3,"a","b","c"]
+#length list
+print(len(B)) # output : 7
+print()  #empty line
 
+#  Access Items
+# List items are indexed and you can access them by referring to the index number:
+
+print(B[2]) # output : 2
+print()  #empty line
+
+#  Negative Indexing
+# Negative indexing means start from the end
+# -1 refers to the last item, -2 refers to the second last item etc.
+
+print(B[-1]) # output : "c"
+print()  #empty line
+
+#  Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+# When specifying a range, the return value will be a new list with the specified items.
+
+print(B[2:6]) # output : [2,3,"a","b"]
+# Note: The search will start at index 2 (included) and end at index 5 (not included).
+print()  #empty line
+
+# Range of Negative Indexes
+# Specify negative indexes if you want to start the search from the end of the list:
+
+print(B[-5:-1]) # output : [2,3,"a","b"]
