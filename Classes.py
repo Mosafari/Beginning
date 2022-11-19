9fd15202990548ea04cb28d4681699916c401b4c
mosafari <m.safari2680@gmail.com>
2022-11-19 18:09:28 +0330

    Classes/Objects

---
+++ Classes.py
@@ -0,0 +1,18 @@
+# Classes/Objects
+
+# Python is an object oriented programming language.
+# Almost everything in Python is an object, with its properties and methods.
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+# Create a Class
+
+# To create a class, use the keyword class:
+
+class A:
+    x=50
+    
+# Create Object
+# Now we can use the class named MyClass to create objects
+
+o1=A()
+
+print(o1.x)
