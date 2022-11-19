961d85a66fb83b345e5a661ce6272aca67373b21
mosafari <m.safari2680@gmail.com>
2022-11-19 16:43:53 +0330

    loop through string

---
+++ Loops/For.py
@@ -0,0 +1,22 @@
+# For Loops
+
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+# This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+# With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+A = "string"
+B = 5 #num
+C = ['a','b','c','d','e','f']
+D = (2,4,6,8,10)
+E = {1,1,2,3,4,5}
+F = {'a':1, 'b':2, 'c':3, 'd':4}
+
+# The for loop does not require an indexing variable to set beforehand.
+
+# loop through string
+for char in A :
+    print(char)
+    
+print() #empty line
+    
+    
