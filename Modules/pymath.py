0c8d73a9bd8bcaf2bfca04c096c6b0e05be7835d
mosafari <m.safari2680@gmail.com>
2022-11-19 19:04:09 +0330

    math

---
+++ Modules/pymath.py
@@ -0,0 +1,24 @@
+# The Math Module
+
+# Python has also a built-in module called math, which extends the list of mathematical functions.
+# To use it, you must import the math module
+
+import math
+
+# When you have imported the math module, you can start using methods and constants of the module.
+# The math.sqrt() method for example, returns the square root of a number
+
+x = math.sqrt(64)
+
+print(x) # 8.0
+
+j = math.ceil(1.4)
+k = math.floor(1.4)
+
+print(j) # returns 2
+print(k) # returns 1 
+
+
+p = math.pi
+
+print(p) 
