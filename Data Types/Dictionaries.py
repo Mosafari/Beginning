02c7981b121412e3e2bbff73799f2b30dc6146b2
mosafari <m.safari2680@gmail.com>
2022-11-19 15:44:05 +0330

    Copy a Dictionary

--- Data Types/Dictionaries.py
+++ Data Types/Dictionaries.py
@@ -119,3 +119,19 @@ print(D)
 print() #empty line
 
 #                                      *** ( loop in Dict )  in loop section ***
+
+# Copy Dictionaries
+
+# Copy a Dictionary
+# You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+# There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+
+A={1:'a',2:'b'}
+D = A.copy()
+print(D)
+print() #empty line
+
+# Another way to make a copy is to use the built-in function dict()
+C = dict(A)
+print(C)
+print() #empty line
