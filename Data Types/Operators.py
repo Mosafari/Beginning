8afb47e05cde1652254c09ef44d1e35c671815d4
mosafari <m.safari2680@gmail.com>
2022-11-19 10:06:08 +0330

    Assignment Operators

--- Data Types/Operators.py
+++ Data Types/Operators.py
@@ -3,9 +3,21 @@
 
 # Python Arithmetic Operators
 
-print(4/2) # output : 2
+print(4/2) # output : 2.0
+print(4//2) # output : 2
 print(6-2) # output : 4
 print(2+4) # output : 6
 print(4*2) # output : 8
 print(4**2) # output : 16
 print()  #empty line
+
+# Python Assignment Operators
+#  Assignment operators are used to assign values to variables:
+x = 4
+print(x) # output : 4
+x+=2 # x = x + 2 -> 4 + 2 = 6  -> x = 6
+print(x) # output : 6
+x-=4 # x = x - 2 -> 6 - 4 = 2 -> x = 2
+print(x) # output : 2
+x*=2 # x = x * 2 -> 2 * 2 = 4 -> x = 4
+print(x) # output : 4
