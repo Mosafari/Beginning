7ae3a51a031370139bff5061c568064afb21cb7e
mosafari <m.safari2680@gmail.com>
2022-11-19 15:18:39 +0330

    Dictionaries

---
+++ Data Types/Dictionaries.py
@@ -0,0 +1,24 @@
+# Dictionaries
+
+# Dictionary
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+# As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+# Dictionaries are written with curly brackets, and have keys and values:
+
+D = {'a': 1, 'b': 2, 'c': 3}
+
+# Dictionary Items
+# Dictionary items are ordered, changeable, and does not allow duplicates.
+# Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
+# Ordered or Unordered?
+# As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+# When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+# Unordered means that the items does not have a defined order, you cannot refer to an item by using an index.
+
+# **Dictionaries cannot have two items with the same key**
+
+print(len(D))
+print(type(D))
+print() #empty line
