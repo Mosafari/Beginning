b27f53f99eb560809a99932982b817c3ef6eb8b3
mosafari <m.safari2680@gmail.com>
2022-11-19 18:40:48 +0330

    Create an Iterator

--- Iterator.py
+++ Iterator.py
@@ -20,3 +20,41 @@ print(next(myit))
 print(next(myit))
 print(next(myit))
 print(next(myit))
+
+
+# Create an Iterator
+
+# To create an object/class as an iterator you have to implement the methods __iter__() and __next__() to your object.
+# As you have learned in the Python Classes/Objects chapter, all classes have a function called __init__(),
+# which allows you to do some initializing when the object is being created.
+# The __iter__() method acts similar, you can do operations (initializing etc.), but must always return the iterator object itself.
+# The __next__() method also allows you to do operations, and must return the next item in the sequence.
+
+# StopIteration
+
+# The example above would continue forever if you had enough next() statements, or if it was used in a for loop.
+# To prevent the iteration to go on forever, we can use the StopIteration statement.
+# In the __next__() method, we can add a terminating condition to raise an error if the iteration is done a specified number of times:
+
+class iterrange:
+  def __init__(self, start, end):
+      self.start = start
+      self.end = end
+      
+  def __iter__(self):
+    self.a = self.start
+    return self
+
+  def __next__(self):
+    if self.a < self.end:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = iterrange(2,10)
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
