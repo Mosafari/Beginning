ff55a26befcff0b49658e3e064fe92731780ffa8
mosafari <m.safari2680@gmail.com>
2022-11-19 16:47:43 +0330

    The range() Function

--- Loops/For.py
+++ Loops/For.py
@@ -19,4 +19,32 @@ for char in A :
     
 print() #empty line
     
+# The range() Function
+# To loop through a set of code a specified number of times, we can use the range() function,
+# The range() function returns a sequence of numbers, starting from 0 by default,
+# and increments by 1 (by default), and ends at a specified number.
+
+for i in range(B):
+    print(i)
     
+print() #empty line
+
+
+
+# Note that range(6) is not the values of 0 to 6, but the values 0 to 5.
+# The range() function defaults to 0 as a starting value,
+# however it is possible to specify the starting value by adding a parameter: range(2, 6),
+# which means values from 2 to 6 (but not including 6):
+
+for i in range(2,9):
+    print(i)
+    
+print() #empty line
+
+# The range() function defaults to increment the sequence by 1,
+# however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+
+for i in range(0,11,2):
+    print(i)
+    
+print() #empty line
