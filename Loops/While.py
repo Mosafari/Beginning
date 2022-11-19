d0aeb417cebcde1c3a24228f562c26acd5cd38b7
mosafari <m.safari2680@gmail.com>
2022-11-19 16:37:31 +0330

    The else Statement

--- Loops/While.py
+++ Loops/While.py
@@ -36,3 +36,13 @@ while i < 6:
     print(i)
   
 print() #empty line
+
+# The else Statement
+# With the else statement we can run a block of code once when the condition no longer is true:
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
