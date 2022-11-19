02f4ee71ee8487638075592c92aba2aa863e2903
mosafari <m.safari2680@gmail.com>
2022-11-19 16:33:58 +0330

    The while Loop

---
+++ Loops/While.py
@@ -0,0 +1,16 @@
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+i = 0
+while i < 10:
+    print(i)
+    i+=1
+    
+print() #empty line
+
+
+
+# Note: remember to increment i, or else the loop will continue forever.
+# The while loop requires relevant variables to be ready,
+# in this example we need to define an indexing variable, i, which we set to 1.
+
