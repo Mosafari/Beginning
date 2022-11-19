8e685689f5cce2d67219ff2ea4f32c650e4bdac4
mosafari <m.safari2680@gmail.com>
2022-11-19 15:36:13 +0330

    Add Dictionary Items

--- Data Types/Dictionaries.py
+++ Data Types/Dictionaries.py
@@ -76,3 +76,17 @@ print() #empty line
 D.update({'newkey': 'value'})
 print(D)
 print() #empty line
+
+# Add Dictionary Items
+
+# Adding Items
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+D["hello"] = "world"
+print(D)
+print() #empty line
+
+# Update Dictionary
+# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+
